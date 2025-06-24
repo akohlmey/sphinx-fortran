03d3d0de6b3e566c7faa6bda744dcba75e60c3d2
Axel Kohlmeyer <akohlmey@gmail.com>
2025-06-24 00:45:57 -0400

    modernize setup.cfg and setup.py to comply with recent setuptools versions

--- setup.py
+++ setup.py
@@ -29,10 +29,6 @@ classifiers = [
         ("License :: OSI Approved :: CEA CNRS Inria Logiciel "
          "Libre License, version 2.1 (CeCILL-2.1)"),
         "Topic :: Scientific/Engineering",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         ]
 
 if __name__ == '__main__':
@@ -52,6 +48,7 @@ if __name__ == '__main__':
         classifiers=classifiers,
         packages=find_packages(),
         install_requires=requires,
+        python_requires='>=3.6',
     )
 
 
